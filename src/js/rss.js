3411388cb0c26e06b9e338d8c41485b3e321fe3a
Xiaomi <surma.artem@gmail.com>
2019-11-19 19:26:17 +0300

    init commit

--- src/js/rss.js
+++ src/js/rss.js
@@ -4,6 +4,6 @@ let parser = new Parser();
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
 export  default  async () => {
-    let feed = await parser.parseURL(CORS_PROXY + 'http://www.reddit.com/.rss');
+    let feed = await parser.parseURL(CORS_PROXY + 'https://laravel.com/docs/6.x/.rss');
     return feed;
 };
